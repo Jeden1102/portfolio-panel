87dee6a0fb802860664032113bfdf741f1397173
jeden1102 <bum123@o2.pl>
2024-04-08 20:55:51 +0200

    master. Possible naive ui load fix.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,41 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-    modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "nuxt-icon",
-        '@nuxtjs/supabase',
-    ],
-    app: {
-        pageTransition: { name: "page", mode: "out-in" },
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "nuxt-icon",
+    "@nuxtjs/supabase",
+  ],
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
+  supabase: {
+    redirect: false,
+  },
+  runtimeConfig: {
+    public: {
+      SUPABASE_URL: "",
+      SUPABASE_KEY: "",
     },
-    supabase: {
-        redirect: false
+  },
+  build: {
+    transpile:
+      process.env.NODE_ENV === "production"
+        ? [
+            "naive-ui",
+            "vueuc",
+            "@css-render/vue3-ssr",
+            "@juggle/resize-observer",
+          ]
+        : ["@juggle/resize-observer"],
+  },
+  vite: {
+    optimizeDeps: {
+      include:
+        process.env.NODE_ENV === "development"
+          ? ["naive-ui", "vueuc", "date-fns-tz/esm/formatInTimeZone"]
+          : [],
     },
-    runtimeConfig: {
-        public: {
-            SUPABASE_URL: '',
-            SUPABASE_KEY: '',
-        }
-    }
+  },
 });
